3f1c05f94159c5b417447383f0e06c38d12ef1fc
DanielTran3 <DTran3@ualberta.ca>
2017-03-04 22:10:02 -0700

    working on insertionsort

--- Insertionsort.c
+++ Insertionsort.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
+
 int* read_data(char *filename) {
 	FILE *fp;
+	int *data_array;
+	data_array = (int *) malloc(sizeof(int));
+	int pos = 0;
+	fp = fopen(filename, "r");
+	fscanf(fp, "%i", data_array[0]);
+	while () {
+		
 }
 
 int * insertionSort() {
